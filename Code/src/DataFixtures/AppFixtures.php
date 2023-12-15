43710ee0c96e8b5a1bb6719dccd44300dbcef14a
lumap <lumap@duck.com>
2023-12-15 14:34:46 +0100

    Update security.yaml, MainController.php, and AppFixtures.php

--- Code/src/DataFixtures/AppFixtures.php
+++ Code/src/DataFixtures/AppFixtures.php
@@ -9,62 +9,63 @@ use App\Entity\User;
 use App\Entity\CommentairesLieu;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
-
 {
     /**@let Generator */
     private Generator $faker;
- 
-    public function __construct()
+
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
     {
+        $this->passwordHasher = $passwordHasher;
         $this->faker = Factory::create('fr_FR');
     }
-    public function load(ObjectManager $manager ): void
+    public function load(ObjectManager $manager): void
     {
         // Users
         $users = [];
-        for ($i=0; $i < 50; $i++) { 
+        for ($i = 0; $i < 50; $i++) {
             $user = new User();
-            $user ->setNom($this->faker->word())
-                  ->setPrenom($this->faker->word())
-                  ->setAvatarUrl($this->faker->word())
-            ->setEmail($this->faker->email)
-            ->setRoles([mt_rand(0, 1) == 1 ? mt_rand(0,1):null])
-            ->setPassword('1234');
+            $user->setNom($this->faker->word())
+                ->setPrenom($this->faker->word())
+                ->setAvatarUrl($this->faker->word())
+                ->setEmail($this->faker->email)
+                ->setRoles(['ROLE_USER']);
+            $user->setPassword($this->passwordHasher->hashPassword($user, 'password'));
+
             $users[] = $user;
-           
             $manager->persist($user);
         }
 
         $date_expiration = new \DateTimeImmutable('now');
 
         // Cadeaux
-        for ($i=0; $i < 50; $i++) { 
+        for ($i = 0; $i < 50; $i++) {
             $cadeaux = new Cadeau();
-            $cadeaux ->setnom($this->faker->word())
-            ->setNomPartenaire($this->faker->word())
-            ->setSiteWebPartenaire($this->faker->url)
-            ->setDateExpiration($date_expiration)
-            ->setDescription($this->faker->text);
-            
-           
+            $cadeaux->setnom($this->faker->word())
+                ->setNomPartenaire($this->faker->word())
+                ->setSiteWebPartenaire($this->faker->url)
+                ->setDateExpiration($date_expiration)
+                ->setDescription($this->faker->text)
+                ->addMembre($users[mt_rand(0, count($users) - 1)]);
 
             $manager->persist($cadeaux);
 
         }
         // Commentaires Lieu
-        for ($i=0; $i < 50; $i++) { 
+        for ($i = 0; $i < 50; $i++) {
             $commentairesLieu = new CommentairesLieu();
-            $commentairesLieu ->setTitre($this->faker->word())
-            ->setDescription($this->faker->text)
-            ->setLieuGps($this->faker->randomNumber())
-            ->setNote(mt_rand(0, 5) == 5 ? mt_rand(0,5):null)
-            ->setMembre($users[mt_rand(0,count($users)-1)]);
+            $commentairesLieu->setTitre($this->faker->word())
+                ->setDescription($this->faker->text)
+                ->setLieuGps($this->faker->randomNumber())
+                ->setNote(mt_rand(0, 5) == 5 ? mt_rand(0, 5) : null)
+                ->setMembre($users[mt_rand(0, count($users) - 1)]);
 
             $manager->persist($commentairesLieu);
         }
-    $manager->flush();
+        $manager->flush();
     }
 }
---
+++ Code/src/Form/RegistrationType.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type as FormType;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', FormType\TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlenght' => '2',
+                    'maxlenght' => '50',
+                ],
+                'label' => 'Nom',
+                'label_attr' => [
+                    'class' => 'form-label  mt-4'
+                ],
+                'constraints' => [
+                    new Assert\NotBlank(),
+                    new Assert\Length(['min' => 2, 'max' => 50])
+                ]
+            ])
+            ->add('prenom', FormType\TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlenght' => '2',
+                    'maxlenght' => '50',
+                ],
+                'label' => 'Prénom',
+                'label_attr' => [
+                    'class' => 'form-label  mt-4'
+                ],
+                'constraints' => [
+                    new Assert\NotBlank(),
+                    new Assert\Length(['min' => 2, 'max' => 50])
+                ]
+            ])
+            ->add('email', FormType\EmailType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlenght' => '2',
+                    'maxlenght' => '180',
+                ],
+                'label' => 'Adresse email',
+                'label_attr' => [
+                    'class' => 'form-label  mt-4'
+                ],
+                'constraints' => [
+                    new Assert\NotBlank(),
+                    new Assert\Email(),
+                    new Assert\Length(['min' => 2, 'max' => 180])
+                ]
+            ]
+            )
+            ->add('password', FormType\RepeatedType::class, [
+                'type' => FormType\PasswordType::class,
+                'first_options' => [
+                    'attr' => [
+                        'class' => 'form-control'
+                    ],
+                    'label' => 'Mot de passe',
+                    'label_attr' => [
+                        'class' => 'form-label  mt-4'
+                    ]
+                ],
+                'second_options' => [
+                    'attr' => [
+                        'class' => 'form-control'
+                    ],
+                    'label' => 'Confirmation du mot de passe',
+                    'label_attr' => [
+                        'class' => 'form-label  mt-4'
+                    ]
+                ],
+                'invalid_message' => 'Les mots de passe ne correspondent pas.'
+            ])
+            ->add('submit', FormType\SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-primary mt-4'
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
