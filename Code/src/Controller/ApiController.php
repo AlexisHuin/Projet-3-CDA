464ac35d12c8249426539fa201b67fa9c7204315
Michael Einfrein <michael.einfrein@campus-centre.fr>
2023-12-19 16:44:55 +0100

    update

--- Code/src/Controller/ApiController.php
+++ Code/src/Controller/ApiController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
-class SearchController extends AbstractController
+class ApiController extends AbstractController
 {
     private $client;
 
