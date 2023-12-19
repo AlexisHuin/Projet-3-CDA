ce949033eafc03d9f3b064242d4eced9e376f156
Michael Einfrein <michael.einfrein@campus-centre.fr>
2023-12-19 16:08:36 +0100

    update api controlller

--- Code/src/Controller/ApiController.php
+++ Code/src/Controller/ApiController.php
@@ -1,47 +1,70 @@
 <?php
 
-    // #[Route('/search', name: 'api.search', methods: ['GET'])]
-    // public function searchApi(string $query)
-    // {
-    //     $apiUrl = 'https://opentripmap-places-v1.p.rapidapi.com/en/places/bbox?lon_max=2.325511&lat_min=46.784633&lon_min=-1.75033&lat_max=48.199482&limit=1000&kinds=historic';
+namespace App\Controller;
 
-    //     try {
-    //         $response = $this->client->request('GET', $apiUrl, [
-    //             'query' => [
-    //                 'q' => $query,
-    //             ],
-    //             'headers' => [
-    //                 'X-RapidAPI-Host' => 'opentripmap-places-v1.p.rapidapi.com',
-    //                 'X-RapidAPI-Key' => 'a0da21aed5mshc79b166d2ac2bbdp1f12a8jsn024a611abaf3',
-    //             ],
-    //         ]);
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
-    //         if ($response->getStatusCode() !== 200) {
-    //             return new JsonResponse(['error' => 'Failed to fetch search results'], $response->getStatusCode());
-    //         }
+class SearchController extends AbstractController
+{
+    private $client;
 
-    //         // Récupérez les données JSON de la réponse
-    //         $data = $response->toArray();
-            
-    //         foreach ($data['features'] as $place) {
-    //             $name = $place['properties']['name'];
-    //             $kinds = $place['properties']['kinds'];
-    //             $web = $place['properties']['rate'] ?? '';
-    //             $preview = $place['properties']['image'] ?? '';
-    //             $info = $place['properties']['wikidata'] ?? '';
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
 
-    //             echo 'Name: ' . $name . '<br>';
-    //             echo 'kinds ' .  $kinds . '<br>';
-    //             echo 'Web: ' . $web . '<br>';
-    //             echo 'Preview: ' . $preview . '<br>';
-    //             echo 'Info: ' . $info . '<br>';
-    //         }
+    #[Route('/search', name: 'api.search', methods: ['GET'])]
+    public function searchApi(Request $request): JsonResponse
+    {
+        $query = $request->query->get('q');
+        $apiUrl = 'https://opentripmap-places-v1.p.rapidapi.com/en/places/bbox?lon_max=2.325511&lat_min=46.784633&lon_min=-1.75033&lat_max=48.199482&limit=1000';
 
-    //         return new JsonResponse(['results' => $data]);
-    //     } catch (\Exception $e) {
-    //         // Gérez les erreurs en fonction de vos besoins
-    //         return new JsonResponse(['error' => 'An error occurred: ' . $e->getMessage()], 500);
-    //     }
-    // }
+        try {
+            $response = $this->client->request('GET', $apiUrl, [
+                'query' => ['q' => $query],
+                'headers' => [
+                    'X-RapidAPI-Host' => 'opentripmap-places-v1.p.rapidapi.com',
+                    'X-RapidAPI-Key' => 'a0da21aed5mshc79b166d2ac2bbdp1f12a8jsn024a611abaf3',
+                ],
+            ]);
 
-?>
+            if ($response->getStatusCode() !== 200) {
+                return new JsonResponse(['error' => 'Failed to fetch search results'], $response->getStatusCode());
+            }
+
+            $data = $response->toArray();
+            $results = $this->processSearchResults($data);
+
+            return new JsonResponse(['results' => $results]);
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => 'An error occurred: ' . $e->getMessage()], 500);
+        }
+    }
+
+    private function processSearchResults(array $data): array
+    {
+        $results = [];
+
+        foreach ($data['features'] as $place) {
+            $name = $place['properties']['name'];
+            $kinds = $place['properties']['kinds'];
+            $web = $place['properties']['rate'] ?? '';
+            $preview = $place['properties']['image'] ?? '';
+            $info = $place['properties']['wikidata'] ?? '';
+
+            $results[] = [
+                'name' => $name,
+                'kinds' => $kinds,
+                'web' => $web,
+                'preview' => $preview,
+                'info' => $info,
+            ];
+        }
+
+        return $results;
+    }
+}
