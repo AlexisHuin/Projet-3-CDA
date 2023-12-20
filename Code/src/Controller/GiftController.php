abfd5b3d94c59ed5508346a5dccb0e6533367338
Alexis Huin <redemption41@gmail.com>
2023-12-20 11:57:19 +0100

    page cadeau non inscrit

--- Code/src/Controller/GiftController.php
+++ Code/src/Controller/GiftController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\CadeauRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GiftController extends AbstractController
 {
     #[Route('/gift', name: 'app_gift')]
-    public function index(): Response
+    public function index(CadeauRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
+        $cadeaux = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1), 
+            10
+        );
+      
         return $this->render('gift/gift.html.twig', [
-            'controller_name' => 'GiftController',
+            'cadeaux' => $cadeaux
         ]);
     }
 }
